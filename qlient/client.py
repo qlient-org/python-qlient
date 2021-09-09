e234df730f9ab3daa57832c22d622f5a34ee8d64
Daniel Seifert <daniel.seifert@swisscom.com>
2021-09-09 21:10:35 +0200

    start client

---
+++ qlient/client.py
@@ -0,0 +1,12 @@
+""" 
+
+:author: Daniel Seifert
+:created: 09.09.2021
+:copyright: Swisscom
+"""
+
+
+class Client:
+    """ This class represents the base qlient Client.
+
+    """
---
+++ tests/__init__.py
@@ -0,0 +1,6 @@
+""" 
+
+:author: Daniel Seifert
+:created: 09.09.2021
+:copyright: Swisscom
+"""
