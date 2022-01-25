6666df14543e75c3f9fcfa462f2f4c6a590638b4
Daniel Seifert <info@danielseifert.ch>
2022-01-25 14:10:24 +0100

    add backend tests

--- tests/test_backend.py
+++ tests/test_backend.py
@@ -1,3 +1,7 @@
+from qlient.types import GraphQLQuery, GraphQLVariables, GraphQLOperation, GraphQLContext, GraphQLRoot, \
+    GraphQLReturnType
+
+
 def test_http_backend():
     from qlient.backend import HTTPBackend
     from requests.sessions import Session
@@ -6,3 +10,27 @@ def test_http_backend():
     assert isinstance(backend.session, Session)
     assert backend.session is not Session()
     assert backend.endpoint == "https://test.test"
+
+
+def test_custom_backend():
+    from qlient.backend import Backend
+
+    class MyBackend(Backend):
+        def execute_query(
+                self,
+                query: GraphQLQuery,
+                variables: GraphQLVariables = None,
+                operation_name: GraphQLOperation = None,
+                context: GraphQLContext = None,
+                root: GraphQLRoot = None
+        ) -> GraphQLReturnType:
+            return {}
+
+        @property
+        def cache_key(self) -> str:
+            return "ABC"
+
+    my_backend = MyBackend()
+
+    assert my_backend.cache_key == "ABC"
+    assert my_backend.execute_query("foo") == {}
