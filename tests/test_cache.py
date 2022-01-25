4d1e0558addbdc4ed3105ce2c6f41c691e1a214f
Daniel Seifert <info@danielseifert.ch>
2022-01-25 14:25:25 +0100

    add cache tests

---
+++ tests/test_cache.py
@@ -0,0 +1,23 @@
+def test_in_memory_cache():
+    from qlient.cache import InMemoryCache
+
+    my_cache = InMemoryCache()
+
+    my_cache["test"] = {"foo": "bar"}
+    assert "test" in my_cache
+
+
+def test_sqlite_cache():
+    from qlient.cache import SqliteCache
+    my_cache = SqliteCache()
+    my_cache["test"] = {"foo": "bar"}
+    assert "test" in my_cache
+
+
+def test_custom_sqlite_cache():
+    import datetime
+    from qlient.cache import SqliteCache
+    my_cache = SqliteCache(path="/tmp/test_cache.sqlite", expires_in=60 * 60 * 24)
+    assert my_cache.path == "/tmp/test_cache.sqlite"
+    assert isinstance(my_cache.expires_in, datetime.timedelta)
+    assert my_cache.expires_in == datetime.timedelta(days=1)
