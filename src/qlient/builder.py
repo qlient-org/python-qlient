932d910eb4251104e2887880c1c487608fc84f1d
Daniel Seifert <info@danielseifert.ch>
2022-03-08 10:21:19 +0100

    chore(PY-D0003): add missing docstrings

--- src/qlient/builder.py
+++ src/qlient/builder.py
@@ -609,13 +609,25 @@ class PreparedFields:
 
 
 class GQLQueryBuilder:
+    """Class to programmatically build graphql queries"""
 
     @staticmethod
     def remove_duplicate_spaces(query: str) -> str:
+        """Static method to remove duplicate spaces from a string
+
+        :param query: holds the string from which to drop the duplicate white spaces
+        :return: a string with no duplicate white spaces
+        """
         return " ".join(query.split())
 
     @staticmethod
     def build_input(variables: Dict[str, Any], initial_str: str) -> str:
+        """Static method to build the input with variables
+
+        :param variables: holds a dictionary with the variable key mapped to the variable value
+        :param initial_str: holds the initial string (prefixed the input)
+        :return: a full-fledged graphql input string
+        """
         inputs: List[str] = []
 
         final_str = initial_str
@@ -656,10 +668,23 @@ class GQLQueryBuilder:
         self.fields_field: Optional[str] = None
 
     def fields(self, fields: str) -> "GQLQueryBuilder":
+        """Method to register a field selection
+
+        Do not put leading and trailing curly brackets
+
+        :param fields: holds the fields as a string (e.g. "name friends { name }")
+        :return: self
+        """
         self.fields_field = fields
         return self
 
     def action(self, action: str, variables: Dict[str, Any] = None) -> "GQLQueryBuilder":
+        """Method to register the graphql action/resolver you wish to execute
+
+        :param action: holds the graphql action/resolver (e.g. getUser)
+        :param variables: holds the action variables
+        :return: self
+        """
         action = self.build_input(variables, action)
         self.action_field = action
         return self
@@ -670,6 +695,13 @@ class GQLQueryBuilder:
             name: str = "",
             variables: Dict[str, Any] = None
     ) -> "GQLQueryBuilder":
+        """Method to register the graphql operation
+
+        :param operation: holds the operation type (query, mutation, subscription)
+        :param name: optional, holds the name of the operation, required for usage with variables
+        :param variables: optional, holds query variables
+        :return: self
+        """
         if name:
             operation = f"{operation} {name}"
             operation = self.build_input(variables, operation)
@@ -677,6 +709,10 @@ class GQLQueryBuilder:
         return self
 
     def build(self) -> str:
+        """Method to build the graphql query
+
+        :return: the complete graphql query with operation, action and fields.
+        """
         query_parts = [self.operation_field, "{", self.action_field]
         if self.fields_field:
             query_parts.append("{")
@@ -691,6 +727,15 @@ class GQLQueryBuilder:
 
 
 class TypedGQLQueryBuilder:
+    """Class that represents a typed GraphQL Query Builder
+
+    The typed graphql builder takes the operation type, operation field, schema and settings
+    as input arguments to ensure type safety in the graphql query.
+
+    The operation_field is the action that you are trying to execute.
+    Say you want to register a new user with your graphql mutation "registerUser".
+    Then a "registerUser" field will be present in the schema.
+    """
 
     def __init__(
             self,
@@ -715,6 +760,15 @@ class TypedGQLQueryBuilder:
         self._action_variables: Optional[Dict[str, Any]] = {}
 
     def fields(self, *args, **kwargs) -> Dict[str, Any]:
+        """Method to programmatically create a type safe field selection.
+
+        This method takes the *args and **kwargs and creates a "Fields" instances.
+        It then prepares this Fields instance.
+
+        :param args: holds an iterable of fields (e.g. "firstname", "lastname", ...)
+        :param kwargs: holds a dictionary for simple deeper field selection.
+        :return: a dictionary with declared variable references mapped to their values
+        """
         self._fields = Fields(*args, **kwargs).prepare(self.op_output, self.schema)
         for var_ref, var_type_ref in self._fields.var_ref_to_var_type.items():
             prefixed_key = f"${var_ref}"
@@ -723,6 +777,11 @@ class TypedGQLQueryBuilder:
         return self._fields.var_ref_to_var_value
 
     def variables(self, **kwargs) -> Dict[str, Any]:
+        """Method to register operation level variables.
+
+        :param kwargs: holds a dictionary with variable key mapped to variable value
+        :return: a dictionary with declared variable references mapped to their values
+        """
         for key in kwargs:
             if self.settings.validate_variables and key not in self.op_inputs:
                 raise KeyError(f"Input `{key}` not supported for {self.op_type} operation `{self.op_name}`")
@@ -734,6 +793,10 @@ class TypedGQLQueryBuilder:
         return kwargs
 
     def build(self) -> str:
+        """Method to build the graphql query string from all given inputs
+
+        :return: the graphql query string
+        """
         if self._fields is not None:
             self.builder.fields(self._fields.__gql__())
         if self._operation_variables is not None:
@@ -743,4 +806,8 @@ class TypedGQLQueryBuilder:
         return self.builder.build()
 
     def __gql__(self) -> str:
+        """Method to build the graphql query string from all given inputs
+
+        :return: the graphql query string
+        """
         return self.build()
