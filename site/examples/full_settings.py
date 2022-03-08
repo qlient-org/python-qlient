226131250e55cda78c8c7185e330aa4e2e72a9d2
Daniel Seifert <info@danielseifert.ch>
2022-03-08 15:30:35 +0100

    docs: rebuild

--- site/examples/full_settings.py
+++ site/examples/full_settings.py
@@ -1,9 +1,13 @@
+import json
+
 from qlient import Settings, Client
 
 my_settings = Settings(
     introspect=True,  # default, enable backend schema introspection
     validate_fields=True,  # default, enable query field selection validation
-    validate_variables=True  # default, enable query variable validation
+    validate_variables=True,  # default, enable query variable validation,
+    json_dumps=json.dumps,  # default, use python's builtin json.dumps for internal dumping
+    json_loads=json.loads,  # default, use python's builtin json.loads for internal loading
 )
 
 my_client = Client("https://...", settings=my_settings)
--- site/examples/simple_client_instantiation.py
+++ site/examples/simple_client_instantiation.py
@@ -2,5 +2,5 @@ from qlient import Client, HTTPBackend, Schema
 
 client = Client("https://swapi-graphql.netlify.app/.netlify/functions/index")
 
-assert isinstance(client.schema, Schema)
-assert isinstance(client.backend, HTTPBackend)
+isinstance(client.schema, Schema)
+isinstance(client.backend, HTTPBackend)
